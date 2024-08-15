9d56fb591d19bd95bbb5d1ea7590d7e6ec4b651e
Luan L <luan.rodrigues@terceiro.rnp.br>
2024-08-15 15:30:35 -0300

    refactor: inicio da remocao das dependencias do camunda

--- src/main/java/it/getinsight/Application.java
+++ src/main/java/it/getinsight/Application.java
@@ -1,30 +1,19 @@
 package it.getinsight;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.camunda.zeebe.spring.client.annotation.Deployment;
 import it.getinsight.core.GetInsightSpringBootApp;
-import it.getinsight.module.request.process.RequestProcess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 
 
 @EnableFeignClients
 @SpringBootApplication
-@Deployment(resources = "classpath*:/bpmn/*.bpmn")
 public class Application extends GetInsightSpringBootApp {
 
     public static void main(String[] args) {
         onApplicationLoaded(SpringApplication.run(Application.class, args));
-    }
-
-
-    @Bean
-    @Lazy(false)
-    public RequestProcess SolicitacaoProcess() {
-        return new RequestProcess();
     }
 
     @Bean
